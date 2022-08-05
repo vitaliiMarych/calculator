550d391f4362539ada85182c636be5fbe242a967
Vitalii Marych <apiskin557@gmail.com>
2022-08-05 17:19:53 +0300

    add file scanner and writer many lines

--- javaCode/src/calcBack.java
+++ javaCode/src/calcBack.java
@@ -2,15 +2,17 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.LinkedList;
 
 public class calcBack {
 
 	public static void main(String[] args) throws Exception {
     	
-		String[] calc = new String[3];
+		LinkedList<String[]> calc = new LinkedList<>();
 		
 		try (Scanner scFile = new Scanner(new File("input.txt"))){
-	    	calc = scFile.nextLine().split(" ");	
+			while (scFile.hasNextLine())
+				calc.addLast(scFile.nextLine().split(" "));	
 	    }
 		
 		catch (Exception e) {
@@ -22,49 +24,49 @@ public class calcBack {
     	String signs = "/*-+";
     	
     	try (FileWriter  pw = new FileWriter ("output.txt", false)) {
-    	
-	    	try {
-	    		
-	    		a = Double.valueOf(calc[0]);
-	    		
-	    		if (signs.indexOf(calc[1]) == -1)
-	    			throw new Exception("Operation Error!");
-	    		
-	    		b = Double.valueOf(calc[2]);
-	    		
-	    		if (b == 0 && calc[1].equals("/"))
-	    			throw new Exception("Error! Division by zero");
-	    		
-	    		switch (calc[1]) {
-	    		
-	    		case ("+"): 
-	    			pw.write(Double.toString(a + b));
-	    			break;
-	    			
-	    		case ("-"): 
-	    			pw.write(Double.toString(a - b));
-	    			break;
-	    		
-	    		case ("/"): 
-	    			pw.write(Double.toString(a / b));
-	    			break;
-	    		
-	    		case ("*"): 
-	    			pw.write(Double.toString(a * b));
-	    			break;
-	    		}
-	    		
-	    	}
-	    	
-	    	catch (NumberFormatException e) {
-	    		pw.write("Error! Not number");
-	    	}
-	    	
-	    	catch (Exception e) {
-	    		pw.write(e.getMessage());
+    		for (String[] c : calc) {
+		    	try {
+		    		
+		    		a = Double.valueOf(c[0]);
+		    		
+		    		if (signs.indexOf(c[1]) == -1)
+		    			throw new Exception("Operation Error!\n");
+		    		
+		    		b = Double.valueOf(c[2]);
+		    		
+		    		if (b == 0 && c[1].equals("/"))
+		    			throw new Exception("Error! Division by zero\n");
+		    		
+		    		switch (c[1]) {
+		    		
+		    		case ("+"): 
+		    			pw.write(Double.toString(a + b)+ "\n");
+		    			break;
+		    			
+		    		case ("-"): 
+		    			pw.write(Double.toString(a - b)+ "\n");
+		    			break;
+		    		
+		    		case ("/"): 
+		    			pw.write(Double.toString(a / b)+ "\n");
+		    			break;
+		    		
+		    		case ("*"): 
+		    			pw.write(Double.toString(a * b) + "\n");
+		    			break;
+		    		}
+		    		
+		    	}
+    		
+		    	catch (NumberFormatException e) {
+		    		pw.write("Error! Not number\n");
+		    	}
+		    	
+		    	catch (Exception e) {
+		    		pw.write(e.getMessage());
+		    	}
 	    	}
     	}
-    	
         catch(IOException e){	
             System.out.println(e.getMessage());
         } 
